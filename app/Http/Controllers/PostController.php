ce814ec21ada6cd293ef827d82c88cd27f67a647
Gianmarco <gianmarco.cavaliere19093@gmail.com>
2021-02-05 11:06:59 +0100

    Mancano delle viste

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Post;
+use App\Tag;
+use App\Category;
+use App\PostInformation;
 
 class PostController extends Controller
 {
@@ -34,6 +37,9 @@ class PostController extends Controller
     public function create()
     {
         //
+        $tags = Tag::all();
+        $categories = Category::all();
+        return view("create", compact("tags", "categories"));
     }
 
     /**
@@ -45,6 +51,36 @@ class PostController extends Controller
     public function store(Request $request)
     {
         //
+        $data = $request->all();
+
+        $validated = $request->validate([
+            'title' => 'required|string|min:3',
+            'author' => 'required|string|min:3',
+        ]);
+
+        $newPost = Post::create([
+            "title" => $validated["title"],
+            "author" => $validated["author"],
+            "category_id" => $data["categories"]
+        ]);
+
+
+        $newPost->save();
+
+        $postInfo = PostInformation::create([
+            "post_id" => $newPost->id,
+            "description" => $data["description"],
+            "slug" => "prova_slug"
+        ]);
+
+        $postInfo->save();
+
+        foreach ($data["tags"] as $tag) {
+            $newPost->tags()->attach($tag);
+        }
+
+
+        return redirect()->route('post');
     }
 
     /**
@@ -56,6 +92,11 @@ class PostController extends Controller
     public function show($id)
     {
         //
+        $post = Post::find($id);
+        $tags = $post->tags;
+        $detail = $post->hasInfo;
+        $category = $post->hasCategory;
+        return view("details", compact("detail", "post", "category", "tags"));
     }
 
     /**
@@ -67,6 +108,11 @@ class PostController extends Controller
     public function edit($id)
     {
         //
+        $post = Post::find($id);
+        $tags = Tag::all();
+        $categories = Category::all();
+
+        return view("edit", compact("post", "tags", "categories"));
     }
 
     /**
@@ -79,6 +125,17 @@ class PostController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $post = Post::find($id);
+        $data = $request->all();
+        $post->tags()->detach();
+        $post->update($data);
+
+        $post->hasInfo->update($data);
+        foreach ($data["tags"] as $tag) {
+            $post->tags()->attach($tag);
+        }
+
+        return redirect()->route('post');
     }
 
     /**
@@ -90,5 +147,16 @@ class PostController extends Controller
     public function destroy($id)
     {
         //
+        $post = Post::find($id);
+        $post->hasInfo->delete();
+
+        foreach ($post->tags as $tag) {
+
+            $post->tags()->detach($tag->id);
+        }
+        $post->delete();
+
+
+        return redirect()->back();
     }
 }
